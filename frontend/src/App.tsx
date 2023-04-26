6a14e6070eab41dfefbe06efb82019f2aaaf2781
geeeeenccc <gencgeray@gmail.com>
2023-04-26 18:52:00 +0300

    Edit App.tsx to clean it from unnecessary elements

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,10 @@
 import React from "react";
+import { MantineProvider, Button } from '@mantine/core';
 
-const user = {
-  name: "Ronnie Coleman",
-  imageUrl:
-    "https://hips.hearstapps.com/hmg-prod/images/mh-3-23-coleman-1648059910.png?crop=0.5xw:1xh;center,top&resize=1200:*",
-  imageSize: 500,
-};
-
-export default function Profile() {
-  return (
-    <>
-      <h1>{user.name}</h1>
-      <img
-        className="avatar"
-        src={user.imageUrl}
-        alt={"Photo of " + user.name}
-        style={{
-          width: user.imageSize,
-          height: user.imageSize,
-        }}
-      />
-    </>
-  );
+export default function App() {
+    return (
+        <MantineProvider theme={{ fontFamily: "Greycliff CF, sans-serif" }}>
+            <Button variant="gradient">Does something</Button>
+        </MantineProvider>
+    );
 }
