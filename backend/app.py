2082cb53e4570331b0c1bfd01701c3f2b5335da0
Герай Генч <101811004+geeeeenccc@users.noreply.github.com>
2023-04-28 20:08:02 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,15 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-
-if __name__ == '__main__':
+"""Module providing Flask server usage""" 
+from flask import Flask 
+ 
+app = Flask(__name__) 
+ 
+ 
+@app.route("/") 
+def hello_world(): 
+    """returns 'Hello World!'""" 
+ 
+    return "Hello World!" 
+ 
+ 
+if name == "__main__": 
     app.run()
