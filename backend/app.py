b24fb0ffbbe454766c72d20df2350f2ee4af0b6b
Герай Генч <101811004+geeeeenccc@users.noreply.github.com>
2023-04-29 17:16:12 +0300

    Update app.py, poetry.lock and pyproject.toml

--- backend/app.py
+++ backend/app.py
@@ -1,28 +1,31 @@
 """Module providing Flask server usage"""
 from flask import Flask, jsonify
-import requests
 
+"""Module providing requests usage"""
+import requests
 
 app = Flask(__name__)
 
 
-@app.route('/get_weather', methods=['GET'])
+@app.route("/get_weather", methods=["GET"])
 def get_weather():
     """Get location and time span
 
     Return json object with weather data for this location and period
     """
 
-    # I have yet to understand how we get the data so.... I'll test dates and location in this commit
+    # I have yet to understand how we get the data so....
+    # I'll test dates and location in this commit
+
     # location = request.args.get('location')
     # start_date = request.args.get('start_date')
     # end_date = request.args.get('end_date')
 
     start_date = "2023-04-29 15:00:00"  # test start date
     end_date = "2023-04-30 12:00:00"  # test end date
-    test_loc = 'Odessa'
+    test_loc = "Odessa"
 
-    api_key = 'cd9b969bbe27d1a7006d234402947f0a'  # API key from openweathermap.org
+    api_key = "cd9b969bbe27d1a7006d234402947f0a"  # API key from openweathermap.org
     base_url = "https://api.openweathermap.org/data/2.5/forecast?"
     complete_url = base_url + "q=" + test_loc + "&appid=" + api_key
 
@@ -30,9 +33,9 @@ def get_weather():
     data = response.json()
 
     weather_data = []
-    for forecast in data['list']:
-        if start_date <= forecast['dt_txt'] <= end_date:
-            weather_data.append(forecast['weather'][0]['description'])
+    for forecast in data["list"]:
+        if start_date <= forecast["dt_txt"] <= end_date:
+            weather_data.append(forecast["weather"][0]["description"])
 
     return jsonify(weather_data)
 
@@ -44,5 +47,5 @@ def hello_world():
     return "Hello World!"
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
