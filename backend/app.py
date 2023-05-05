cf781f0c59fbfef059f354c4646e122b24c2c7f2
Герай Генч <101811004+geeeeenccc@users.noreply.github.com>
2023-05-05 15:06:01 +0300

    Add /describe_weather endpoint to app.py

--- backend/app.py
+++ backend/app.py
@@ -7,13 +7,14 @@ from datetime import datetime, timedelta
 from flask import Flask, request
 import requests
 from dotenv import load_dotenv
-
+import openai
 
 app = Flask(__name__)
 load_dotenv()
 
 OPENWEATHERMAP_API_KEY = os.environ.get("OPENWEATHERMAP_API_KEY")
 OPENWEATHERMAP_API_URL = os.environ.get("OPENWEATHERMAP_API_URL")
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 
 
 @app.route("/get_weather", methods=["GET"])
@@ -61,6 +62,61 @@ def get_weather():
         if start_date <= datetime.fromtimestamp(forecast["dt"]) <= end_date:
             weather_data.append(forecast)
     return weather_data, 200
+
+
+@app.route('/describe_weather', methods=['GET'])
+def describe_weather():
+    """
+    This endpoint generates a description of the weather
+    for a given location and time period.
+
+    Returns:
+        A JSON object containing the ai-generated description.
+    """
+    location = request.args.get('location')
+    days_str = request.args.get("days")
+
+    if not location:
+        return 'Parameter location is missing.', 400
+
+    if not days_str:
+        return "Parameter days is missing.", 400
+    try:
+        days = int(days_str)
+        if days <= 0 or days >= 15:
+            return "Parameter days can only take values between 1 and 14.", 400
+    except ValueError:
+        return "Parameter days must be an integer.", 400
+
+    start_date = datetime.now()
+    end_date = (start_date + timedelta(days=days + 1)).replace(
+        hour=3, minute=0, second=0
+    )
+
+    get_weather_response = requests.get(
+        f'http://localhost:5000/get_weather?location={location}&days={days}'
+    )
+    # if get_weather_response.status_code() != 200: whatever
+    weather_data = get_weather_response.json()
+
+    if not weather_data:
+        return 'Parameter weather data is missing.', 400
+
+    openai.api_key = OPENAI_API_KEY
+    prompt = f"Describe the weather in {location} from {start_date} to {end_date}. " \
+             f"The weather data is as follows: {weather_data}."
+    openai_response = openai.Completion.create(
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.7,
+        model="text-davinci-002"
+    )
+    # if openai_response.status_code() != 200: whatever
+    description = openai_response.choices[0].text
+
+    return description, 200
 
 
 @app.route("/")
