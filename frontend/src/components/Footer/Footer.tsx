4a15082e27caa355f6b856885cf26088e033f325
geeeeenccc <gencgeray@gmail.com>
2023-07-27 16:52:07 +0300

    Create FooterStyles.ts to store Footer styles

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,17 +1,12 @@
 import React from "react";
 import { Footer } from "@mantine/core";
+import { footerStyles } from "./FooterStyles";
 
 const CustomFooter: React.FC = () => {
+  const { classes } = footerStyles();
+
   return (
-    <Footer
-      height={40}
-      p="md"
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <Footer height={40} className={classes.footer}>
       GG VK YB ©
     </Footer>
   );
---
+++ frontend/src/components/Footer/FooterStyles.ts
@@ -0,0 +1,10 @@
+import { createStyles } from "@mantine/core";
+
+export const footerStyles = createStyles((theme) => ({
+  footer: {
+    padding: theme.spacing.md,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+}));
